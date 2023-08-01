9535722a0318ad598edd9bb1bb4fef4fba61f78c
Ella <akhilomeella@gmail.com>
2023-08-01 16:03:55 +0100

    updated rating component

---
+++ interactive-rating-component/script.js
@@ -0,0 +1,18 @@
+const mainSection = document.querySelector(".container");
+const subSection = document.querySelector(".secondState");
+const submitButton = document.getElementById("onSubmit");
+let score;
+
+document.querySelectorAll(".btns button").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    score = e.target.textContent;
+  });
+});
+
+submitButton.addEventListener("click", () => {
+  subSection.classList.remove("hide");
+  mainSection.style.display = "none";
+  document.getElementById("rating").innerHTML = `
+      ${score}
+      `;
+});
