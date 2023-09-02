125dee70f6293bb1210f792cdddeba14969ffbc4
akhilomeella <akhilomeella@gmail.com>
2023-09-02 15:28:50 +0100

    removed attribution

--- interactive-rating-component/script.js
+++ interactive-rating-component/script.js
@@ -1,25 +1,23 @@
 const mainSection = document.querySelector(".container");
 const subSection = document.querySelector(".secondState");
 const submitButton = document.getElementById("onSubmit");
+const btns = document.querySelectorAll(".btns button");
 let score;
 
-document.querySelectorAll(".btns button").forEach((btn) => {
+btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     score = e.target.textContent;
   });
 });
 
-if(score !== 0){
-
 submitButton.addEventListener("click", () => {
-  subSection.classList.remove("hide");
-  mainSection.style.display = "none";
-  document.getElementById("rating").innerHTML = `
+  if (score !== undefined) {
+    subSection.classList.remove("hide");
+    mainSection.style.display = "none";
+    document.getElementById("rating").innerHTML = `
       ${score}
       `;
+  } else {
+    alert("Please choose a rating");
+  }
 });
-}
-else{
-  alert("Please choose a rating");
-}
- 
