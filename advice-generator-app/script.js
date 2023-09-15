91db2a73ba6b9b7d2a9defdb1adae8677636cc44
akhilomeella <akhilomeella@gmail.com>
2023-09-15 23:56:09 +0100

    added project

---
+++ advice-generator-app/script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  const adviceID = document.getElementById("adviceID");
+  const adviceText = document.getElementById("adviceText");
+  const Button = document.getElementById("button");
+
+  async function getAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      const quote = data.slip;
+      adviceID.textContent = `${quote.id}`;
+      adviceText.textContent = `"${quote.advice}"`;
+    } catch (error) {
+      console.error(`Error fetching data: ${error.message}`);
+    }
+  }
+
+  Button.addEventListener("click", getAdvice);
+};
